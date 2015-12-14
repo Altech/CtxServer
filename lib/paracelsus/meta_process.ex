0aebea625b4cc7bb068029030fff4e0523631012
Altech <takeno.sh@gmail.com>
2015-12-14 22:39:09 +0900

    Add exec/0 and change from meta/0 to meta/4

--- lib/paracelsus/meta_process.ex
+++ lib/paracelsus/meta_process.ex
@@ -1,11 +1,17 @@
 defmodule Paracelsus.MetaProcess do
 
-  def spawn do
-    Kernel.spawn(&meta1/0)
+  def spawn(f) do
+    exec = Kernel.spawn(&exec/0)
+    Kernel.spawn(meta1([], f, :dormant, exec))
   end
 
-  defp meta1 do
-    IO.puts "foo"
+  defp meta1(queue, function, state, exec) do
+    fn ->
+      IO.puts "foo"
+    end
+  end
+
+  defp exec do
   end
 
 end
