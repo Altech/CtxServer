1df74799c53e71a1c3d505136c40350f2f0877f3
Altech <takeno.sh@gmail.com>
2015-12-15 20:45:27 +0900

    Refactor single meta process

--- lib/paracelsus/meta_process.ex
+++ lib/paracelsus/meta_process.ex
@@ -2,58 +2,16 @@ defmodule Paracelsus.MetaProcess do
 
   import Kernel, except: [self: 0, send: 2, spawn: 1]
 
+  defp kernel_receive(fun) do
+    receive do
+      m -> fun.(m)
+    end
+  end
+
+  # Overriden Primitives
+
   def spawn(fun) do
-    exec = Kernel.spawn(__MODULE__, :exec, [])
-    meta = Kernel.spawn(meta_single([], fun, :dormant, exec))
-    Kernel.send(meta, {:init})
-    meta
-  end
-
-  def meta_single(queue, fun, state, exec) do
-    fn ->
-      receive do
-        {:init} ->
-          Kernel.send(exec, {:apply, fun, nil, Kernel.self()})
-          meta_single(queue, fun, :active, exec).()
-        {:receive, fun} ->
-          IO.puts "{:receive, _}"
-          case queue do
-            [message|queue] ->
-              Kernel.send(exec, {:apply, fun, message, Kernel.self()})
-              meta_single(queue, fun, :active,  exec).()
-            _ ->
-              meta_single(queue, fun, :dormant, exec).()
-          end
-        {:message, message} ->
-          IO.puts "{:message, _} # state: #{state}"
-          case state do
-            :dormant ->
-              Kernel.send(exec, {:apply, fun, message, Kernel.self()})
-              meta_single(queue, fun, :active, exec).()
-            :active ->
-              meta_single(queue ++ [message], fun, :active, exec).()
-          end
-        {:exit} ->
-          IO.puts "{:exit}"
-        x ->
-          raise "meta-single received unexpected message: #{inspect x}"
-      end
-    end
-  end
-
-  def exec do
-    receive do # This is special form of `receive`.
-      {:apply, fun, message, meta} ->
-        Process.put(:self, meta)
-        if message do
-          fun.(message)
-        else
-          fun.()
-        end
-        Kernel.send(meta, {:exit})
-      x ->
-        raise "meta-single received unexpected message: #{inspect x}"
-    end
+    Kernel.spawn(__MODULE__, :meta_single_init, [fun])
   end
 
   def self do
@@ -68,11 +26,75 @@ defmodule Paracelsus.MetaProcess do
   end
 
   def receive(fun) do
-    Kernel.send(Process.get(:self), {:receive, fun})
-    exec()
+    case Process.get(:self) do
+      nil -> kernel_receive(fun)
+      meta ->
+        Kernel.send(meta, {:receive, fun})
+        kernel_receive(exec())
+    end
   end
 
   def my_receive(fun) do
     __MODULE__.receive(fun)
+  end
+
+  # Single Meta Process
+
+  def meta_single_init(fun) do
+    exec = Kernel.spawn(__MODULE__, :exec_init, [])
+    Kernel.send(Kernel.self(), {:init})
+    kernel_receive(meta_single([], fun, :dormant, exec))
+  end
+
+  def meta_single(queue, fun, state, exec) do
+    fn
+      {:init} ->
+        IO.puts "{:init}"
+        Kernel.send(exec, {:apply, fun, nil, Kernel.self()})
+        kernel_receive(meta_single(queue, fun, :active, exec))
+      {:receive, fun} ->
+        IO.puts "{:receive, _}"
+        case queue do
+          [message|queue] ->
+            Kernel.send(exec, {:apply, fun, message, Kernel.self()})
+            kernel_receive(meta_single(queue, fun, :active, exec))
+          _ ->
+            kernel_receive(meta_single(queue, fun, :dormant, exec))
+        end
+      {:message, message} ->
+        IO.puts "{:message, _} # state: #{state}"
+        case state do
+          :dormant ->
+            Kernel.send(exec, {:apply, fun, message, Kernel.self()})
+            kernel_receive(meta_single(queue, fun, :active, exec))
+          :active ->
+            kernel_receive(meta_single(queue ++ [message], fun, :active, exec))
+        end
+      {:exit} ->
+        IO.puts "{:exit}"
+      x ->
+        raise "meta-single received unexpected message: #{inspect x}"
+    end
+  end
+
+  # Execution Process
+
+  def exec_init do
+    kernel_receive(exec())
+  end
+
+  def exec do
+    fn 
+      {:apply, fun, message, meta} ->
+        Process.put(:self, meta)
+        if message do
+          fun.(message)
+        else
+          fun.()
+        end
+        Kernel.send(meta, {:exit})
+      x ->
+        raise "meta-single received unexpected message: #{inspect x}"
+    end
   end
 end
