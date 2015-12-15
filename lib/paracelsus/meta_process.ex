e9e9839dc87e1d10af1e8a5771286eca3a41bcf1
Altech <takeno.sh@gmail.com>
2015-12-15 18:28:01 +0900

    Receive :receive and :message by meta process

--- lib/paracelsus/meta_process.ex
+++ lib/paracelsus/meta_process.ex
@@ -1,9 +1,11 @@
 defmodule Paracelsus.MetaProcess do
 
+  import Kernel, except: [self: 0, send: 2]
+
   def spawn(function) do
     exec = Kernel.spawn(__MODULE__, :exec, [])
     meta = Kernel.spawn(meta_single([], function, :dormant, exec))
-    send(meta, {:init})
+    Kernel.send(meta, {:init})
     meta
   end
 
@@ -13,6 +15,24 @@ defmodule Paracelsus.MetaProcess do
         {:init} ->
           Kernel.send(exec, {:apply, function, nil, Kernel.self()})
           meta_single(queue, function, :active, exec).()
+        {:receive, fun} ->
+          IO.puts "{:receive, _}"
+          case queue do
+            [message|queue] ->
+              Kernel.send(exec, {:apply, fun, message, Kernel.self()})
+              meta_single(queue, fun, :active,  exec).()
+            _ ->
+              meta_single(queue, fun, :dormant, exec).()
+          end
+        {:message, message} ->
+          IO.puts "{:message, _} # state: #{state}"
+          case state do
+            :dormant ->
+              Kernel.send(exec, {:apply, function, message, Kernel.self()})
+              meta_single(queue, function, :active, exec).()
+            :active ->
+              meta_single(queue ++ [message], function, :active, exec).()
+          end
         {:end} ->
           IO.puts "exit!"
         x ->
@@ -22,7 +42,7 @@ defmodule Paracelsus.MetaProcess do
   end
 
   def exec do
-    receive do
+    receive do # This is special form of `receive`.
       {:apply, function, message, meta} ->
         Process.put(:self, meta)
         if message do
@@ -31,6 +51,8 @@ defmodule Paracelsus.MetaProcess do
           function.()
         end
         Kernel.send(meta, {:end})
+      x ->
+        raise "meta-single received unexpected message: #{inspect x}"
     end
   end
 
@@ -39,5 +61,14 @@ defmodule Paracelsus.MetaProcess do
       nil -> Kernel.self()
       val -> val
     end
+  end
+
+  def send(dest, message) do
+    Kernel.send(dest, {:message, message})
+  end
+
+  def receive(fun) do
+    Kernel.send(Process.get(:self), {:receive, fun})
+    exec()
   end
 end
