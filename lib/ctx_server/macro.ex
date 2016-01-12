95d8228af4d3f7864700308f5b996817a6d2173a
Altech <takeno.sh@gmail.com>
2016-01-12 20:02:06 +0900

    Fix index

--- lib/ctx_server/macro.ex
+++ lib/ctx_server/macro.ex
@@ -1,6 +1,6 @@
 defmodule CtxServer.Macro do
   defmacro context(contexts, do: block) do
-    elem Macro.prewalk(block, contexts, &modify_handers_ast/2), 1
+    elem Macro.prewalk(block, contexts, &modify_handers_ast/2), 0
   end
 
   defp modify_handers_ast({:def, meta1, [{name, meta2, args}, [do: body]]}, contexts)
