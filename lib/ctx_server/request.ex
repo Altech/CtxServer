f2c4f33d8bb016c9c2f677bec99feac56aa31422
Altech <takeno.sh@gmail.com>
2016-01-12 20:19:47 +0900

    Don't piggyback local context

--- lib/ctx_server/request.ex
+++ lib/ctx_server/request.ex
@@ -6,6 +6,17 @@ defmodule CtxServer.Request do
   defstruct body: nil, contexts: %{}
 
   def new(body) do
-    %__MODULE__{body: body, contexts: CtxServer.Contexts.current_values} # [TODO] Send only global contexts
+    %__MODULE__{body: body, contexts: global_contexts}
+  end
+
+  defp global_contexts do
+    if Code.ensure_loaded?(ApplicationContext) do
+      for {name, context} <- CtxServer.Contexts.current_values,
+      ApplicationContext.scope(name) == :global, into: %{} do
+        {name, context}
+      end
+    else
+      CtxServer.Contexts.current_values
+    end
   end
 end
